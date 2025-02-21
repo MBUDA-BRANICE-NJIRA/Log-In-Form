07e72bfaf92d1e11af5dc773169ddd37a7e9a4dc
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-21 09:40:14 +0300

    Started worikin on the reidter form section

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -33,6 +33,35 @@ const LoginRegister = () => {
            </div>
         </form>
        </div>
+
+       {/* /* REGISTER SECTION */ }
+
+       <div className="form-box register">
+        <form action="">
+          <h1>Login</h1>
+           
+           <div className="input-box">
+            <input type="text" placeholder='Username' required />
+            <FaUser className='icon'/>
+           </div>
+
+           <div className="input-box">
+            <input type="password" placeholder='Password' required />
+            <FaLock  className='icon'/>
+           </div>
+
+           <div className="remember-forgot">
+            <label><input type="checkbox" />Remember me</label>
+            <a href="nav#">Forgot Password</a>
+           </div>
+
+           <button type='submit'>Login</button>
+
+           <div className="register-link">
+            <p>Don't have an account?<a href="nav#">Register</a></p>
+           </div>
+        </form>
+       </div>
       </div>
   )
 };
