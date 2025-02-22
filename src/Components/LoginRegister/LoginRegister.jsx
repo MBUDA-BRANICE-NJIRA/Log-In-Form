2defeee441abcfbc7d78db329f08388c107fc551
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-22 08:51:41 +0300

    Worked on the jx code redability

--- src/Components/LoginRegister/LoginRegister.jsx
+++ src/Components/LoginRegister/LoginRegister.jsx
@@ -8,73 +8,73 @@ const LoginRegister = () => {
 
   const [action, setAction] = useState('');
 
-  const registerLink =()=>{
+  const registerLink = () => {
     setAction('active');
   };
 
-  const loginLink =()=>{
+  const loginLink = () => {
     setAction('');
   };
 
 
   return (
-    <div className={`Wrapper ${action}`}>
-       <div className="form-box login">
+    <div className = {`Wrapper ${action}`}>
+       <div className = "form-box login">
         <form action="">
           <h1>Login</h1>
            
-           <div className="input-box">
+           <div className = "input-box">
             <input type="text" placeholder='Username' required />
-            <FaUser className='icon'/>
+            <FaUser className = 'icon'/>
            </div>
 
-           <div className="input-box">
-            <input type="password" placeholder='Password' required />
-            <FaLock  className='icon'/>
+           <div className ="input-box">
+            <input type="password" placeholder = 'Password' required />
+            <FaLock  className = 'icon'/>
            </div>
 
-           <div className="remember-forgot">
-            <label><input type="checkbox" />Remember me</label>
+           <div className = "remember-forgot">
+            <label><input type = "checkbox" />Remember me</label>
             <a href="nav#">Forgot Password</a>
            </div>
 
-           <button type='submit'>Login</button>
+           <button type = 'submit'>Login</button>
 
-           <div className="register-link">
-            <p>Don't have an account? <a href="" onClick={registerLink}>Register</a></p>
+           <div className = "register-link">
+            <p>Don't have an account? <a href = "#" onClick = {registerLink}>Register</a></p>
            </div>
         </form>
        </div>
 
        {/* /* REGISTER SECTION */ }
 
-       <div className="form-box register">
-        <form action="">
+       <div className ="form-box register">
+        <form action = "#">
           <h1>Registration</h1>
            
-           <div className="input-box">
-            <input type="text" placeholder='Username' required />
-            <FaUser className='icon'/>
+           <div className ="input-box">
+            <input type = "text" placeholder='Username' required />
+            <FaUser className ='icon'/>
            </div>
 
-           <div className="input-box">
-            <input type="email" placeholder='Email' required />
+           <div className ="input-box">
+            <input type="email" placeholder = 'Email' required />
             <FaEnvelope className='icon'/>
            </div>
 
            <div className="input-box">
-            <input type="password" placeholder='Create Password' required />
-            <FaLock  className='icon'/>
+            <input type = "password" placeholder = 'Create Password' required />
+            <FaLock  className ='icon'/>
            </div>
 
-           <div className="remember-forgot">
-            <label><input type="checkbox" />I agree to the terms & conditions.</label>
+           <div className = "remember-forgot">
+            <label><input type = "checkbox" />I agree to the terms & conditions.</label>
            </div>
 
-           <button type='submit'>Register</button>
+           <button type =' submit'>Register</button>
 
-           <div className="register-link">
-            <p>Alredy have an account? <a href="" onClick={loginLink}>Login</a></p>
+           <div className ="register-link">
+            <p>Alredy have an account? <a href = "#" onClick = {loginLink}>Login</a></p>
            </div>
         </form>
        </div>
